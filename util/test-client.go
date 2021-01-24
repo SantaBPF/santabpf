13b7628db1e631d1b8318d80203f65b4509cd24e
Jung Dong Ho <dongho971220@gmail.com>
2021-01-24 19:12:17 +0000

    add cli flags to test-client

--- util/test-client.go
+++ util/test-client.go
@@ -1,28 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
 type client struct {
 	remote_addr string
+	remote_port string
+	wg          sync.WaitGroup
 }
 
 func (c *client) Get(seconds float64) {
-	resp, _ := http.Get(fmt.Sprintf("http://%v:38500/delay?seconds=%v", c.remote_addr, seconds))
+	defer c.wg.Done()
+
+	start := time.Now()
+
+	resp, _ := http.Get(fmt.Sprintf("http://%v:%v/delay?seconds=%v", c.remote_addr, c.remote_port, seconds))
 	rawBody, _ := ioutil.ReadAll(resp.Body)
 	body := string(rawBody)
 
-	fmt.Println(body)
+	fmt.Println(body, fmt.Sprintf("[%v in client]", time.Since(start)))
 }
 
 func main() {
-	c := client{os.Getenv("TEST_SERVER_ADDR")}
+	flagSync := flag.Bool("sync", false, "send requests synchronously")
+	flagN := flag.Int("n", 1, "how many requests you want")
+	flagDelay := flag.Float64("delay", 0, "delay in seconds")
 
-	c.Get(3)
-	c.Get(6)
+	flag.Parse()
+
+	remote_addr := os.Getenv("TEST_SERVER_ADDR")
+	remote_port := os.Getenv("TEST_SERVER_PORT")
+
+	c := client{remote_addr, remote_port, sync.WaitGroup{}}
+
+	for i := 0; i < *flagN; i++ {
+		if *flagSync {
+			c.Get(*flagDelay)
+		} else {
+			c.wg.Add(1)
+			go c.Get(*flagDelay)
+		}
+	}
+
+	c.wg.Wait()
 }
