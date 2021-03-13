afee3090a4ca652d01ac825afa24caba7208e06b
Jung Dong Ho <dongho971220@gmail.com>
2021-03-13 23:41:16 +0900

    add autologging, util.shell

---
+++ src/util/shell.py
@@ -0,0 +1,24 @@
+import re
+import subprocess
+
+
+def run(cmd, *, timeout):
+    return _parse_table(_run(cmd, timeout=timeout))
+
+
+def raw_run(cmd, *, timeout):
+    return _run(cmd, timeout=timeout)
+
+
+def _run(cmd, *, timeout=None):
+    return subprocess.check_output(['/bin/sh', '-c', cmd], timeout=timeout, text=True,
+                                   stderr=subprocess.STDOUT).splitlines()
+
+
+def _parse_table(output):
+    def to_number(x): float(re.sub('[^0-9.]', '', x))
+
+    header = output[0].split()
+    body = (map(to_number, o.split()) for o in output[1:])
+
+    return dict(zip(header, zip(*body)))
