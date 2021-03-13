94bfe9500f505e1cf044613c2d821fad015a381b
Jung Dong Ho <dongho971220@gmail.com>
2021-03-13 23:41:16 +0900

    add autologging, util.shell

--- src/scenarios/passive/insufficient_page_cache.py
+++ src/scenarios/passive/insufficient_page_cache.py
@@ -1,13 +1,18 @@
 import logging
+from statistics import mean
+
+from autologging import traced
 
 from scenarios._base import Scenario
-from util import prom
+from util import prom, shell
 
 
+@traced
 class InsufficientPageCache(Scenario):
     def monitor(self):
         if prom.query('avg_over_time(netdata_disk_util___of_time_working_average[5s]) > 90', '3m'):
             self.troubleshoot()
 
     def troubleshoot(self):
-        pass
+        cachestat = shell.run('cachestat 2 5')
+        logging.info(mean(cachestat['HITRATIO']))
