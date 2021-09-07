6b780f9c2791bf5a7af62ceb963b2b0526120e47
Jung Dong Ho <dongho971220@gmail.com>
2021-09-07 04:03:29 +0000

    send.py 유니코드 이슈 수정

--- netdata/santabpf/exec/scenarios/system_cpu.py
+++ netdata/santabpf/exec/scenarios/system_cpu.py
@@ -4,9 +4,15 @@ from base import Event, exec_bpftrace
 from send import send_email
 
 def _1min_cpu_usage(event: Event):
-    msg = exec_bpftrace('profile:hz:99 { @[pid, comm] = count(); }', ['pid', 'comm', 'count'], '@\[(.+?), (.+?)\]: (.+?)$', 3)
+    profile = exec_bpftrace('profile:hz:99 { @[pid, comm] = count(); }', ['pid', 'comm', 'count'], '@\[(.+?), (.+?)\]: (.+?)$', 3)
+
+    msg = f'최근 1분간 cpu 사용량이 비정상적으로 높아짐. 다음은 3초간 샘플링한 (pid, comm)당 on-cpu count\n\n{profile}\n\n TODO(drilling down)'
+
     send_email('dongho971220@gmail.com', repr(event), msg)
 
 def _1min_steal_cpu(event: Event):
-    msg = exec_bpftrace('profile:hz:99 { @[pid, comm] = count(); }', ['pid', 'comm', 'count'], '@\[(.+?), (.+?)\]: (.+?)$', 3)
+    profile = exec_bpftrace('profile:hz:99 { @[pid, comm] = count(); }', ['pid', 'comm', 'count'], '@\[(.+?), (.+?)\]: (.+?)$', 3)
+
+    msg = f'TODO\n\n{profile}'
+
     send_email('dongho971220@gmail.com', repr(event), msg)
--- netdata/santabpf/exec/send.py
+++ netdata/santabpf/exec/send.py
@@ -8,13 +8,18 @@ password = os.getenv('AUTH_PASS')
 
 def send_email(recipient, title, content):
     message = f"""Subject: {title}
+MIME-Version: 1.0
+Content-type: text/html; charset=utf-8
 
 <pre style="font: monospace">
 {content}
 </pre>
-    """
+    """.encode('utf8')
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, recipient, message)
+
+if __name__ == '__main__':
+    send_email('dongho971220@gmail.com', 'test', '테스트 메시지 test message')
