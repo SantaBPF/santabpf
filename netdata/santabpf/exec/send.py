bb7afa06a0a6f73359097275f8e2e91c2137461a
Jung Dong Ho <dongho971220@gmail.com>
2021-09-08 04:40:52 +0900

    exec 코드 정리

--- netdata/santabpf/exec/send.py
+++ netdata/santabpf/exec/send.py
@@ -1,10 +1,12 @@
 import os
-import smtplib, ssl
+import smtplib
+import ssl
 
 smtp_server = "smtp.gmail.com"
 port = 465
 sender_email = os.getenv('AUTH_USER')
 password = os.getenv('AUTH_PASS')
+
 
 def send_email(recipient, title, content):
     message = f"""Subject: {title}
@@ -20,6 +22,7 @@ Content-type: text/html; charset=utf-8
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, recipient, message)
+
 
 if __name__ == '__main__':
     send_email('dongho971220@gmail.com', 'test', '테스트 메시지 test message')
